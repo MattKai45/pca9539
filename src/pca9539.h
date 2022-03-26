5ad557383fc956f315a8753a23bd20461e55abcc
Matt Kaiser <MatthewKaiser45@gmail.com>
2022-03-26 08:42:38 -0400

    Add I2C address reference
    Table 8-2 of TI PCA9539 datasheet (March 2021)

--- src/pca9539.h
+++ src/pca9539.h
@@ -8,6 +8,15 @@
 #ifndef PCA9539_H
 #define PCA9539_H
 
+/**
+* PCA9539 7-bit base address. This should be shifted by 1 bit to include the R/W bit.
+* The base address would appear as 0xE8 for read and 0xE9 for write. If the A0-A2 pins
+* are pulled high the appropriate bit should be set. Example,If A1 is high:
+* PCA9539_ADDR = PCA9539_7BIT_BASE_ADDRESS | PCA9539_A1.
+*/
+#define PCA9539_7BIT_BASE_ADDRESS (uint8_t)0x74
+#define PCA9539_A0                (uint8_t)(1 << 0)
+#define PCA9539_A1                (uint8_t)(1 << 1)
 #endif /* PCA9539_H */
 
 /*** end of file ***/
